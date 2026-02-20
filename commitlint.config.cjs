a6e395e6a627eb03f1aafca3117b70713e03f054
Dede Fuji Abdul <defuj.dev@gmail.com>
2026-02-20 10:09:32 +0700

    fix: increase header max length to accommodate longer commit messages

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -21,5 +21,6 @@ module.exports = {
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-max-length': [2, 'always', 100],
     'body-max-line-length': [2, 'always', 200],
+    'header-max-length': [2, 'always', 2000],
   },
 }
